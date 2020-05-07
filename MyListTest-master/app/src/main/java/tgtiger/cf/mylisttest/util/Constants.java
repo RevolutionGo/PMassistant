bd67aa0f4e72d06ad010a81455e1c358ee79248b
RevolutionGo <32586385+RevolutionGo@users.noreply.github.com>
2020-05-07 19:19:24 +0800

    Update Constants.java

--- MyListTest-master/app/src/main/java/tgtiger/cf/mylisttest/util/Constants.java
+++ MyListTest-master/app/src/main/java/tgtiger/cf/mylisttest/util/Constants.java
@@ -3,10 +3,11 @@ package tgtiger.cf.mylisttest.util;
 /**
  * 常量类
  * 用来一些常量
+ * 本地伪登录账号和密码
  */
 public class Constants {
-    public static final String USERNAME = "13066870138";
-    public static final String PASSWORD = "807318819bb";
+    public static final String USERNAME = "130668*****";
+    public static final String PASSWORD = "1234567890";
 
     public static final String ID = "ID";
 }
